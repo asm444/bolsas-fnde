27e97b2fe3f43bafae4426362bd3abc160c11531
Arthur de Souza Molina <arthur.souza.molina@gmail.com>
2024-01-16 13:31:47 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bolsasfnde',
-    version='0.1',
+    version='2.0',
     packages=find_packages(),
     install_requires=['requests'],
     entry_points={
