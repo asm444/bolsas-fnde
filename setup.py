235aeba8bd88576ff21bc960523fbe93de57d6bd
Arthur de Souza Molina <arthur.souza.molina@gmail.com>
2024-01-16 23:27:22 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bolsasfnde',
-    version='2.0',
+    version='3.2',
     packages=find_packages(),
     install_requires=['requests'],
     entry_points={
@@ -10,4 +10,7 @@ setup(
             'bolsasfnde = bolsasfnde.__main__:main',
         ],
     },
-)
+    python_requires='>=3.6',
+    description="A biblioteca automatiza consultas periódicas ao site do FNDE para verificar se a tão esperada bolsa foi concedida. Adeus às verificações manuais repetitivas!",
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown')
